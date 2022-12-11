4dc2684b3bc2b787b64a493a3e5197789a1d89e6
Abner Silveira de Freitas <defreitasabner@gmail.com>
2022-12-11 17:02:49 -0300

    Classe Planilha já está lendo os dados de uma planilha.

--- Planilha.py
+++ Planilha.py
@@ -4,9 +4,9 @@ from typing import List, Dict
 class Planilha:
     
     def __init__(self, caminho_csv: str) -> None:
-        self.dados: List[Dict[str, str]] = self.extrair_dados(caminho_csv)
+        self._dados: List[Dict[str, str]] = self.extrair_dados(caminho_csv)
 
-    def extrair_dados(caminho_csv: str) -> List[Dict[str, str]]:
+    def extrair_dados(self, caminho_csv: str) -> List[Dict[str, str]]:
 
         try:
 
@@ -14,7 +14,7 @@ class Planilha:
 
             with open(caminho_csv, 'r', encoding='utf-8') as planilha:
                 
-                dados_planilha = csv.DictReader(planilha)
+                dados_planilha = csv.DictReader(planilha, delimiter=',', skipinitialspace=True)
                 
                 for dado in dados_planilha:
                     dados.append(dado)
@@ -24,3 +24,12 @@ class Planilha:
         except Exception:
             
             raise Exception('Ocorreu um erro ao abrir a planilha...')
+            
+    @property
+    def dados(self) -> List[Dict[str, str]]:
+        return self._dados
+
+    @dados.setter
+    def dados(self, caminho_novos_dados: str) -> None:
+        novos_dados = self.extrair_dados(caminho_novos_dados)
+        self._dados = novos_dados

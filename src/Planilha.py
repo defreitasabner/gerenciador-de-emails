21492e60929b4d0615ea5b5444201242a2eba530
Abner Freitas <defreitasabner@gmail.com>
2022-12-21 15:35:29 -0300

    Comentando classe Planilha e adicionando validação de colunas da planilha.

--- src/Planilha.py
+++ src/Planilha.py
@@ -1,10 +1,16 @@
 import csv
 from typing import List, Dict
 
+from constantes import LISTA_COLUNAS_ESPERADAS
+
 #TODO: Adicionar verificador dos campos presentes na planilha, devem ser sempre os mesmos
 class Planilha:
     
     def __init__(self, caminho_csv: str) -> None:
+        """
+        Classe responsável por gerenciar os arquivos `.csv`. O método construtor de `Planilha` recebe um caminho (`str`) para um arquivo `.csv` e armazena as informações obtidas no atributo `dados`, na forma de uma lista de dicionários.
+        """
+        self.colunas_esperadas: List[str] = LISTA_COLUNAS_ESPERADAS
         self._dados: List[Dict[str, str]] = self.extrair_dados(caminho_csv)
 
     def extrair_dados(self, caminho_csv: str) -> List[Dict[str, str]]:
@@ -20,6 +26,8 @@ class Planilha:
                 for dado in dados_planilha:
                     dados.append(dado)
 
+            self.validar_colunas(dados)
+
             return dados
         
         except Exception:
@@ -28,9 +36,30 @@ class Planilha:
             
     @property
     def dados(self) -> List[Dict[str, str]]:
+        """
+        Método responsável pelo acesso aos dados do objeto `Planilha`. Retorna os dados na forma de uma lista com dicionários, no qual cada dicionário representa uma linha da planilha e as chaves são os títulos das colunas e os valores são referentes aos valores daquela linha.
+        """
         return self._dados
 
     @dados.setter
     def dados(self, caminho_novos_dados: str) -> None:
+        """
+        Método responsável pela troca de dados do objeto `Planilha`. Apenas para o caso de querermos carregar novos dados vindos de outro arquivo `.csv`. Os dados antigos serão substituídos na memória.
+        """
         novos_dados = self.extrair_dados(caminho_novos_dados)
         self._dados = novos_dados
+
+    def validar_colunas(self, dados: List[Dict[str, str]]) -> None:
+        """
+        Método responsável por verificar se todos os dicionários extraídos estão com as chaves correspondentes as colunas esperadas do arquivo `.csv`. Vai retornar erro tanto se planilha estiver sem as colunas apropriadas, quanto se ocorreu algum erro durante a extração de algum dos dicionários.
+        """
+        try:
+
+            for dado in dados:
+                if(list(dado.keys()) == self.colunas_esperadas):
+                    print('OK')
+                else:
+                    raise Exception('Erro ')
+        
+        except:
+            raise Exception('Erro')
---
+++ src/constantes.py
@@ -0,0 +1,10 @@
+# Contastantes da Classe Planilha
+LISTA_COLUNAS_ESPERADAS = [
+    'id',
+    'nome',
+    'email',
+    'status_formulario',
+    'status_dinamica',
+    'status_entrevista',
+    'status_trainee'
+]

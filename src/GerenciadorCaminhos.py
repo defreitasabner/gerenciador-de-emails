ab5aca3f30733de6f7efec494a30632479161855
Abner Freitas <defreitasabner@gmail.com>
2022-12-22 16:15:06 -0300

    Refatorada a Classe GerenciadorCaminhos.

--- src/GerenciadorCaminhos.py
+++ src/GerenciadorCaminhos.py
@@ -5,15 +5,17 @@ from exception import ErroDiretorioDataNaoEncontrado, ErroDiretorioPlanilhasNaoE
 
 class GerenciadorCaminhos:
     """
+    GerenciadorCaminhos
+    ---
     Classe responsável por gerenciar os caminhos (`paths`) para os diretórios e arquivos importantes para o funcionamento do código. Carrega os `paths` presentes em `config.py` ou carrega os novos caminhos definidos pelo usuário.
     """
 
     def __init__(self) -> None:
-        self.dir_data = Path(DIRETORIO_DATA)
-        self.dir_planilhas = Path(DIRETORIO_DATA, DIRETORIO_PLANILHAS)
-        self.dir_mensagens = Path(DIRETORIO_DATA, DIRETORIO_MSG)
+        self.__dir_data = Path(DIRETORIO_DATA)
+        self.__dir_planilhas = Path(DIRETORIO_DATA, DIRETORIO_PLANILHAS)
+        self.__dir_mensagens = Path(DIRETORIO_DATA, DIRETORIO_MSG)
 
-    def verifica_diretorios(self):
+    def verificar_diretorios(self):
         """
         Método responsável por verificar se os diretórios `data/`, `data/planilhas/` ou `data/mensagens/` existem. Caso não existam, dispara uma `Exception`.
         """
@@ -23,6 +25,37 @@ class GerenciadorCaminhos:
             raise ErroDiretorioPlanilhasNaoEncontrado('ERRO: Diretório "data/planilhas/" não foi encontrado')
         if not self.dir_mensagens.exists():
             raise ErroDiretorioMensagensNaoEncontrado('ERRO: Diretório "data/mensagens/" não foi encontrado')
+        print('Todos os diretórios nos locais corretos!')
 
-    def verifica_arquivos(self):
+    def verificar_arquivos(self):
         pass
+
+    """
+    Métodos Getters e Setters
+    ===
+    Os métodos abaixo servem apenas para acessarmos (getters) e alterarmos (setters) os valores dos atributos privados da classe.
+    """
+
+    @property
+    def dir_data(self):
+        return self.__dir_data
+
+    @dir_data.setter
+    def dir_data(self, novo_caminho):
+        self.dir_data = Path(novo_caminho)
+    
+    @property
+    def dir_planilhas(self):
+        return self.__dir_planilhas
+
+    @dir_planilhas.setter
+    def dir_planilhas(self, novo_caminho):
+        self.dir_planilhas = Path(novo_caminho)
+    
+    @property
+    def dir_mensagens(self):
+        return self.__dir_mensagens
+
+    @dir_mensagens.setter
+    def dir_mensagens(self, novo_caminho):
+        self.dir_mensagens = Path(novo_caminho)
